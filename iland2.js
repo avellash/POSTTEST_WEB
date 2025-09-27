30ae85b699b88a18ce34f263b1fffedddcf7b9f2
avellash <avellash.tik@gmail.com>
2025-09-28 00:06:58 +0800

    ralat pt 3

--- iland2.js
+++ iland2.js
@@ -35,21 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Detail Peserta:\n" + nama);
     });
   });
-
-  async function fetchAnime() {
-    try {
-      const res = await fetch("https://api.jikan.moe/v4/top/anime");
-      const data = await res.json();
-      const beritaSection = document.querySelector("#berita ol");
-      const extra = document.createElement("li");
-      extra.innerHTML = `<h3>Rekomendasi Anime</h3>
-        <p>Anime populer saat ini: <strong>${data.data[0].title}</strong></p>`;
-      beritaSection.appendChild(extra);
-    } catch (error) {
-      console.error("Gagal fetch API:", error);
-    }
-  }
-  fetchAnime();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
